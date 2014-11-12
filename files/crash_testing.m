75c0929f6b26c8a0b59dc822692a9d03b54072ce
JimHokanson <jim.hokanson@gmail.com>
2014-11-11 21:00:40 -0500

    working on isolating source of crashes

---
+++ files/crash_testing.m
@@ -0,0 +1,23 @@
+%Running this code enough will cause the program to crash
+%
+%   I think this suggests that if the file is somehow open in on place that
+%   it shouldn't be opened again in another. 
+%
+%   Suggested fix: When trying to open a file, see if it is in memory.
+%   Problem: I'm not sure how to hold onto the file and yet not allow the
+%   object to be destroyed.
+
+EXPT_ID = '141010_J';
+c = dba.GSK.stim_cmg_expt(EXPT_ID);
+pres_data = c.getData('pres');
+c2 = dba.GSK.stim_cmg_expt(EXPT_ID);
+pres_data2 = c2.getData('pres');
+
+clear c
+clear c2
+clear pres_data
+clear pres_data2
+
+EXPT_ID = '141010_J';
+c = dba.GSK.stim_cmg_expt(EXPT_ID);
+pres_data = c.getData('pres');
